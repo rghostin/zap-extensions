e60cc1277c184171888fee1710e78771b93992df
lucaslulusong@gmail.com <lucaslulusong@gmail.com>
2020-11-07 12:50:55 +0100

    cookie rule

---
+++ zaproxy/zap/src/test/java/org/zaproxy/zap/extension/policyloader/rules/CookieAttrRuleTest.java
@@ -0,0 +1,20 @@
+package org.zaproxy.zap.extension.policyloader.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//TODO
+class CookieAttrRuleTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void isViolated() {
+    }
+}
