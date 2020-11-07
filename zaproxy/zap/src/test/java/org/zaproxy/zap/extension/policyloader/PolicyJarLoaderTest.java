5c256752eb9fd96b3c5b396cf4d473b638213e5f
rghostin <rawadghostin@protonmail.com>
2020-11-07 14:32:48 +0200

    test jar loader

---
+++ zaproxy/zap/src/test/java/org/zaproxy/zap/extension/policyloader/PolicyJarLoaderTest.java
@@ -0,0 +1,17 @@
+package org.zaproxy.zap.extension.policyloader;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// todo test
+class PolicyJarLoaderTest {
+
+    @Test
+    void getPolicyName() {
+    }
+
+    @Test
+    void getRules() {
+    }
+}
