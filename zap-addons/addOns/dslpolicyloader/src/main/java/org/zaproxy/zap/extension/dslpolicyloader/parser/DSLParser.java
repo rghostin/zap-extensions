ab38472bb8a7bbf5c4ff070fd04dc397f477f971
rghostin <rawadghostin@protonmail.com>
2020-11-17 14:30:54 +0200

    DSL placeholder

---
+++ zap-addons/addOns/dslpolicyloader/src/main/java/org/zaproxy/zap/extension/dslpolicyloader/parser/DSLParser.java
@@ -0,0 +1,16 @@
+package org.zaproxy.zap.extension.dslpolicyloader.parser;
+
+import org.zaproxy.zap.extension.dslpolicyloader.Policy;
+import org.zaproxy.zap.extension.dslpolicyloader.Rule;
+
+public class DSLParser {
+    Rule parseRule(String ruleDsl){
+        return null;
+    }
+
+    Policy parsePolicy(String policyContent) {
+        //
+        return null;
+    }
+
+}
