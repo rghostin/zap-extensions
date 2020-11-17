2794a05ce59b06ea881138a4ef70221de9c33e43
Xinhai Zou <xinhai.zou@hotmail.com>
2020-11-17 16:58:21 +0100

    test_token2

--- zap-addons/addOns/dslpolicyloader/src/test/java/org/zaproxy/zap/extension/dslpolicyloader/parser/TokenizerTest.java
+++ zap-addons/addOns/dslpolicyloader/src/test/java/org/zaproxy/zap/extension/dslpolicyloader/parser/TokenizerTest.java
@@ -46,8 +46,8 @@ class TokenizerTest {
 
         assertTrue(tokens.get(0).isSimplePredicate());
         predicate = (Predicate<HttpMessage>) tokens.get(0).getTokenObj();
-        assertTrue(predicate.test(createHttpMsg("abc", "")));
-        assertFalse(predicate.test(createHttpMsg("", "")));
+        assertTrue(predicate.test(createHttpMsg("Request", "abc", "")));
+        assertFalse(predicate.test(createHttpMsg("Request", "", "")));
 
         assertTrue(tokens.get(1).isOperator());
         if (tokens.get(1).isOperator()) {
@@ -63,8 +63,8 @@ class TokenizerTest {
 
         assertTrue(tokens.get(4).isSimplePredicate());
         predicate = (Predicate<HttpMessage>) tokens.get(4).getTokenObj();
-        assertTrue(predicate.test(createHttpMsg("def", "")));
-        assertFalse(predicate.test(createHttpMsg("", "")));
+        assertTrue(predicate.test(createHttpMsg("Response", "def", "")));
+        assertFalse(predicate.test(createHttpMsg("Response", "", "")));
 
         assertTrue(tokens.get(5).isOperator());
         if (tokens.get(5).isOperator()) {
@@ -73,18 +73,32 @@ class TokenizerTest {
 
         assertTrue(tokens.get(6).isSimplePredicate());
         predicate = (Predicate<HttpMessage>) tokens.get(6).getTokenObj();
-        assertTrue(predicate.test(createHttpMsg("", "x")));
-        assertTrue(predicate.test(createHttpMsg("", "yz")));
-        assertFalse(predicate.test(createHttpMsg("", "")));
+        assertTrue(predicate.test(createHttpMsg("Response", "", "x")));
+        assertTrue(predicate.test(createHttpMsg("Response", "", "y")));
+        assertTrue(predicate.test(createHttpMsg("Response", "", "xy")));
+        assertFalse(predicate.test(createHttpMsg("Response", "", "")));
 
         assertTrue(tokens.get(7).isClosedParenthesis());
     }
 
-    private HttpMessage createHttpMsg(String head, String body) {
+    private HttpMessage createHttpMsg(String transmission, String head, String body) {
         try {
             HttpMessage msg = new HttpMessage(new URI("http://example.com/", true));
-
-            msg.setRequestBody(String.format("<html><head>%s</head><body>%s</body><html>", head, body));
+            if ("Request".equals(transmission)) {
+                if (!"".equals(head.trim())) {
+                    // TODO:
+                    msg.getRequestHeader().setHeader("abc", "abc");
+                } else if (!"".equals(body.trim())) {
+                    msg.setRequestBody(String.format("<html><head></head><body>%s</body><html>", body));
+                }
+            } else if ("Response".equals(transmission)) {
+                if (!"".equals(head.trim())) {
+                    // TODO:
+                    msg.getResponseHeader().setHeader("abc", "abc");
+                } else if (!"".equals(body.trim())) {
+                    msg.setResponseBody(String.format("<html><head></head><body>%s</body><html>", body));
+                }
+            }
             return msg;
         } catch (Exception e) {
             e.printStackTrace();
@@ -92,9 +106,3 @@ class TokenizerTest {
         return null;
     }
 }
-
-
-
-//if (token.isOperator()) {
-//        Operator op = (Operator) token.getTokenObj()
-//        }
