8e1eb801a70314b482cc586c11e4ccb69b9cc48b
rghostin <rawadghostin@protonmail.com>
2020-11-17 15:09:45 +0200

    parseRule

--- zap-addons/addOns/dslpolicyloader/src/main/java/org/zaproxy/zap/extension/dslpolicyloader/parser/DSLParser.java
+++ zap-addons/addOns/dslpolicyloader/src/main/java/org/zaproxy/zap/extension/dslpolicyloader/parser/DSLParser.java
@@ -1,20 +1,47 @@
 package org.zaproxy.zap.extension.dslpolicyloader.parser;
 
+import org.parosproxy.paros.network.HttpMessage;
 import org.zaproxy.zap.extension.dslpolicyloader.Policy;
 import org.zaproxy.zap.extension.dslpolicyloader.Rule;
 
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// todo test
 public class DSLParser {
+    private static final String RE_RULE_DECLARATION = "^rule\\s+\"(.+?)\"\\s+\"(.+?)\"\\s*:\\s*(.+)$";
+    private static final Pattern PATTERN_RULE_DECLARATION = Pattern.compile(RE_RULE_DECLARATION);
 
+    // todo test
 
+    /**
+     * Parses a rule declaration as defined in the DSL
+     * @param ruleDsl : the rule declaration
+     * @return : A rule object representing the rule declaration
+     */
     Rule parseRule(String ruleDsl){
-        //re = "rule\s+"(.+?)"\s+"(.+?)"\s*:";
-        //parse on ; and delegate to sttmt parser
-        return null;
+        Matcher ruleMatcher = PATTERN_RULE_DECLARATION.matcher(ruleDsl);
+        boolean matches = ruleMatcher.matches();
+        assert matches;
+        String name = ruleMatcher.group(1);
+        String description = ruleMatcher.group(2);
+        String composeodStatement = ruleMatcher.group(3);
+
+        Predicate<HttpMessage> predicate = new StatementParser(composeodStatement).parse();
+        return new Rule(name, description, predicate);
     }
 
     Policy parsePolicy(String policyContent) {
-        //
+        // for (strign representing rule) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        DSLParser dslParser = new DSLParser();
+
+        dslParser.parseRule("rule \"myrule\" \"mydesc\" : \n request.header.re=\"abc\" and not ( response.header.value=\"def\" or response.body.values=[\"x\",\"y\",\"z\"] )");
+
     }
 
 }
