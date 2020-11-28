020c0d36d55d75da62450fdb9f12bbaafc9cf836
dalpsavaskan <dalpsavaskan@gmail.com>
2020-11-28 14:00:38 +0100

    threshol_test

---
+++ zap-addons/addOns/reportingproxy/src/test/java/org/zaproxy/zap/extension/reportingproxy/rules/ThresholdRuleTest.java
@@ -0,0 +1,20 @@
+package org.zaproxy.zap.extension.reportingproxy.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThresholdRuleTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void isViolated() {
+    }
+}
