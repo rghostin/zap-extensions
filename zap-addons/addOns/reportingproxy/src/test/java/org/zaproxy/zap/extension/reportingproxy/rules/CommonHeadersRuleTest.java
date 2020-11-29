663da36e582ec053eeb0f88a0ff2523e5043173b
Xinhai Zou <xinhai.zou@hotmail.com>
2020-11-29 13:50:09 +0100

    common header code+test fix

--- zap-addons/addOns/reportingproxy/src/test/java/org/zaproxy/zap/extension/reportingproxy/rules/CommonHeadersRuleTest.java
+++ zap-addons/addOns/reportingproxy/src/test/java/org/zaproxy/zap/extension/reportingproxy/rules/CommonHeadersRuleTest.java
@@ -34,107 +34,122 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CommonHeadersRuleTest {
 
-//    private CommonHeadersRule commonHeadersRule;
-//    private HttpMessage httpRespMsg1;
-//    private HttpMessage httpRespMsg2;
-//    private HttpMessage httpRespMsg3;
-//
-//    @BeforeEach
-//    void setup() {
-//        commonHeadersRule = new CommonHeadersRule();
-//        HttpHeaderField field1_1 = new HttpHeaderField("Accept", "text/html");
-//        HttpHeaderField field1_2 = new HttpHeaderField("Accept-Charset", "utf-8");
-//        HttpHeaderField field1_3 = new HttpHeaderField("Accept-Language", "en-US");
-//        HttpHeaderField field1_4 = new HttpHeaderField("Cookie", "sessionId=abc123");
-//        HttpHeaderField field2_1 = new HttpHeaderField("Accept", "application/xml");
-//        HttpHeaderField field2_2 = new HttpHeaderField("Accept-Charset", "GBK");
-//        HttpHeaderField field2_3 = new HttpHeaderField("Accept-Language", "zh;en-US");
-//
-//
-//        List<HttpHeaderField> listForMsg1 = new ArrayList<>();
-//        List<HttpHeaderField> listForMsg2 = new ArrayList<>();
-//        List<HttpHeaderField> listForMsg3 = new ArrayList<>();
-//
-//        listForMsg1.add(field1_1);
-//        listForMsg1.add(field1_2);
-//        listForMsg1.add(field1_3);
-//
-//        listForMsg2.addAll(listForMsg1);
-//        listForMsg2.add(field1_4);
-//
-//        listForMsg3.add(field2_1);
-//        listForMsg3.add(field2_2);
-//        listForMsg3.add(field2_3);
-//
-//        // For creating of first HttpMessage
-//        httpRespMsg1 = createHttpRespMsg(listForMsg1);
-//        // For creating of second HttpMessage
-//        httpRespMsg2 = createHttpRespMsg(listForMsg2);
-//        // For creating of third HttpMessage
-//        httpRespMsg3 = createHttpRespMsg(listForMsg3);
-//    }
-//
-//    @Test
-//    void getName() {
-//        assertEquals("Common_Headers_Rule", commonHeadersRule.getName());
-//    }
-//
-//    @Test
-//    void getDescription() {
-//        assertEquals(
-//                "The HTTP response message does not contain common response header " +
-//                        "present in previous requests."
-//                , commonHeadersRule.getDescription());
-//    }
-//
-//    @Test
-//    void updateBufferWith() {
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg2));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        List<HttpResponseHeader> container =
-//                commonHeadersRule.getHttpResponseHeaderContainer();
-//        assertTrue(container.contains(httpRespMsg2.getResponseHeader()));
-//        commonHeadersRule.checkViolation(httpRespMsg1);
-//        assertFalse(container.contains(httpRespMsg2.getResponseHeader()));
-//    }
-//
-//    @Test
-//    void isViolatedTrue() {
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//
-//        Violation v = commonHeadersRule.checkViolation(httpRespMsg3);
-//        assertNull(v);
-//    }
-//
-//    @Test
-//    void isViolatedFalse() {
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg2));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//
-//        assertNull(commonHeadersRule.checkViolation(httpRespMsg1));
-//    }
-//
-//    private HttpMessage createHttpRespMsg(List<HttpHeaderField> headers) {
-//        try {
-//            HttpMessage msg = new HttpMessage(new URI("http://example.com/", true));
-//            for (HttpHeaderField header : headers) {
-//                msg.getResponseHeader().setHeader(header.getName(), header.getValue());
-//            }
-//            return msg;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
+    private CommonHeadersRule commonHeadersRule;
+    private HttpMessage httpRespMsg1_1;
+    private HttpMessage httpRespMsg1_2;
+    private HttpMessage httpRespMsg1_3;
+    private HttpMessage httpRespMsg1_4;
+    private HttpMessage httpRespMsg1_5;
+    private HttpMessage httpRespMsg2;
+    private HttpMessage httpRespMsg3;
+
+    @BeforeEach
+    void setup() {
+        commonHeadersRule = new CommonHeadersRule();
+        HttpHeaderField field1_1 = new HttpHeaderField("Accept", "text/html");
+        HttpHeaderField field1_2 = new HttpHeaderField("Accept-Charset", "utf-8");
+        HttpHeaderField field1_3 = new HttpHeaderField("Accept-Language", "en-US");
+        HttpHeaderField field1_4 = new HttpHeaderField("Cookie", "sessionId=abc123");
+        HttpHeaderField field2_1 = new HttpHeaderField("Accept", "application/xml");
+        HttpHeaderField field2_2 = new HttpHeaderField("Accept-Charset", "GBK");
+        HttpHeaderField field2_3 = new HttpHeaderField("Accept-Language", "zh;en-US");
+
+
+        List<HttpHeaderField> listForMsg1 = new ArrayList<>();
+        List<HttpHeaderField> listForMsg2 = new ArrayList<>();
+        List<HttpHeaderField> listForMsg3 = new ArrayList<>();
+
+        listForMsg1.add(field1_1);
+        listForMsg1.add(field1_2);
+        listForMsg1.add(field1_3);
+
+        listForMsg2.addAll(listForMsg1);
+        listForMsg2.add(field1_4);
+
+        listForMsg3.add(field2_1);
+        listForMsg3.add(field2_2);
+        listForMsg3.add(field2_3);
+
+        // For creating of first HttpMessage
+        httpRespMsg1_1 = createHttpRespMsg(listForMsg1);
+        httpRespMsg1_2 = createHttpRespMsg(listForMsg1);
+        httpRespMsg1_3 = createHttpRespMsg(listForMsg1);
+        httpRespMsg1_4 = createHttpRespMsg(listForMsg1);
+        httpRespMsg1_5 = createHttpRespMsg(listForMsg1);
+        // For creating of (first-concluded) second HttpMessage
+        httpRespMsg2 = createHttpRespMsg(listForMsg2);
+        // For creating of (first-different) third HttpMessage
+        httpRespMsg3 = createHttpRespMsg(listForMsg3);
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Common_Headers_Rule", commonHeadersRule.getName());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals(
+                "The HTTP response message does not contain common response header " +
+                        "present in previous requests."
+                , commonHeadersRule.getDescription());
+    }
+
+    @Test
+    void updateBufferWith() {
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg2));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_1));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_2));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_3));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_4));
+        List<HttpResponseHeader> container =
+                commonHeadersRule.getHttpResponseHeaderContainer();
+        assertTrue(container.contains(httpRespMsg2.getResponseHeader()));
+        commonHeadersRule.checkViolation(httpRespMsg1_5);
+        container = commonHeadersRule.getHttpResponseHeaderContainer();
+        assertFalse(container.contains(httpRespMsg2.getResponseHeader()));
+    }
+
+    @Test
+    void isViolatedTrue() {
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_1));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_2));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_3));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_4));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_5));
+
+        Violation v = commonHeadersRule.checkViolation(httpRespMsg3);
+
+        assertEquals("Common_Headers_Rule", v.getRuleName());
+        assertEquals("The HTTP response message does not contain common response header "
+                + "present in previous requests.", v.getDescription());
+        assertEquals("http://example.com/", v.getUri());
+        assertEquals("Rule_Common_Headers_Rule violated", v.getTitle());
+        assertEquals(httpRespMsg3, v.getTriggeringMsg());
+    }
+
+    @Test
+    void isViolatedFalse() {
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_1));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_2));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg2));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_3));
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_4));
+
+        assertNull(commonHeadersRule.checkViolation(httpRespMsg1_5));
+    }
+
+    private HttpMessage createHttpRespMsg(List<HttpHeaderField> headers) {
+        try {
+            HttpMessage msg = new HttpMessage(new URI("http://example.com/", true));
+            for (HttpHeaderField header : headers) {
+                msg.getResponseHeader().setHeader(header.getName(), header.getValue());
+            }
+            return msg;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
